b48b73ae7fef4161c5892c5cc14384410648f88e
Preetam Shingavi <pshingavi@expedia.com>
2016-07-15 13:54:40 -0700

    Bean definition inheritence - Property inheritence

--- SpringDemo/src/com/expedia/pshingavi/springdemo/DrawingApplication.java
+++ SpringDemo/src/com/expedia/pshingavi/springdemo/DrawingApplication.java
@@ -9,7 +9,8 @@ public class DrawingApplication {
 		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));	// File spring.xml is at project root
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
 		// File spring.xml is at src root, since we are using Class path app context
-		Triangle triangle = ctx.getBean("triangle", Triangle.class);
+		Triangle triangle = ctx.getBean("triangle1", Triangle.class);
+		//Triangle triangle = ctx.getBean("triangle2", Triangle.class);	// Gives a NPE since triangle2 is not init with pointC
 		triangle.draw();
 	}
 
