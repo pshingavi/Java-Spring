c2fe8cd21711249032cb448ef9fb27b9e9b20f10
Preetam Shingavi <pshingavi@expedia.com>
2016-07-15 12:51:01 -0700

    Autowire byName

--- SpringDemo/src/com/expedia/pshingavi/springdemo/Triangle.java
+++ SpringDemo/src/com/expedia/pshingavi/springdemo/Triangle.java
@@ -1,22 +1,38 @@
 package com.expedia.pshingavi.springdemo;
 
-import java.util.List;
-
 public class Triangle {
 
-	private List<Point> points;
-		
-	public List<Point> getPoints() {
-		return points;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public void setPoints(List<Point> points) {
-		this.points = points;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
 	public void draw() {
-		for(Point point : points) {
-			System.out.println(point.getX() + " :::: " + point.getY());
-		}
+		System.out.println(this.getPointA().getX() + " :::: " + this.getPointA().getY());
+		System.out.println(this.getPointB().getX() + " :::: " + this.getPointB().getY());
+		System.out.println(this.getPointC().getX() + " :::: " + this.getPointC().getY());
 	}
 }
