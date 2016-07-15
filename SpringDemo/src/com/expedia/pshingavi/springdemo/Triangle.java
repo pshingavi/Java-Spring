d11ef6116c5a5bbb161c2cc7e73a1f0788d6e992
Preetam Shingavi <pshingavi@expedia.com>
2016-07-15 12:27:19 -0700

    Object injection

--- SpringDemo/src/com/expedia/pshingavi/springdemo/Triangle.java
+++ SpringDemo/src/com/expedia/pshingavi/springdemo/Triangle.java
@@ -2,39 +2,37 @@ package com.expedia.pshingavi.springdemo;
 
 public class Triangle {
 
-	private String type;
-	private int height;
-	
-	public Triangle(String type) {
-		this.type = type;
-	}
-	
-	public Triangle(int height) {
-		this.height = height;
-	}
-	
-	public Triangle(String type, int height) {
-		this.type = type;
-		this.height = height;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public String getType() {
-		return type;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
 
+	public Point getPointB() {
+		return pointB;
+	}
 
-/*	
- * For using the <constructor-arg>
- * public void setType(String type) {
-		this.type = type;
-	}*/
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
 
+	public Point getPointC() {
+		return pointC;
+	}
 
-	public int getHeight() {
-		return height;
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
 	public void draw() {
-		System.out.println("Triangle type : " + this.type + "Triangle height : " + this.height);
+		System.out.println(this.getPointA().getX() + " :::: " + this.getPointA().getY());
+		System.out.println(this.getPointB().getX() + " :::: " + this.getPointB().getY());
+		System.out.println(this.getPointC().getX() + " :::: " + this.getPointC().getY());
 	}
 }
