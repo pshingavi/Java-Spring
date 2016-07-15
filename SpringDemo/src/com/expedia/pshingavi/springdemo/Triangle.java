564ec8d67254e6881c1ae98bf41665481d9a74ac
Preetam Shingavi <pshingavi@expedia.com>
2016-07-15 13:47:39 -0700

    Bean scopes, ApplicationcontextAware, BeanNameAware

--- SpringDemo/src/com/expedia/pshingavi/springdemo/Triangle.java
+++ SpringDemo/src/com/expedia/pshingavi/springdemo/Triangle.java
@@ -1,11 +1,19 @@
 package com.expedia.pshingavi.springdemo;
 
-public class Triangle {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle implements ApplicationContextAware, BeanNameAware{
 
 	private Point pointA;
 	private Point pointB;
 	private Point pointC;
-
+	private String beanName;
+	
+	private ApplicationContext ctx = null;
+	
 	public Point getPointA() {
 		return pointA;
 	}
@@ -34,5 +42,18 @@ public class Triangle {
 		System.out.println(this.getPointA().getX() + " :::: " + this.getPointA().getY());
 		System.out.println(this.getPointB().getX() + " :::: " + this.getPointB().getY());
 		System.out.println(this.getPointC().getX() + " :::: " + this.getPointC().getY());
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext ctx) throws BeansException {
+		this.ctx = ctx;
+	}
+
+	@Override
+	public void setBeanName(String beanName) {
+		this.beanName = beanName;
+		System.out.println(this.ctx);	// prints null
+		System.out.println("Bean name is : " + this.beanName);
+		
 	}
 }
