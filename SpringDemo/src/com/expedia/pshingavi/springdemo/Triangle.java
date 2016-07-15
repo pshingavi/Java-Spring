05f5c2d5cb43b35cfe21aea7502e13a1283c48c2
Preetam Shingavi <pshingavi@expedia.com>
2016-07-15 12:47:43 -0700

    Initializing collections

--- SpringDemo/src/com/expedia/pshingavi/springdemo/Triangle.java
+++ SpringDemo/src/com/expedia/pshingavi/springdemo/Triangle.java
@@ -1,38 +1,22 @@
 package com.expedia.pshingavi.springdemo;
 
+import java.util.List;
+
 public class Triangle {
 
-	private Point pointA;
-	private Point pointB;
-	private Point pointC;
-
-	public Point getPointA() {
-		return pointA;
+	private List<Point> points;
+		
+	public List<Point> getPoints() {
+		return points;
 	}
 
-	public void setPointA(Point pointA) {
-		this.pointA = pointA;
-	}
-
-	public Point getPointB() {
-		return pointB;
-	}
-
-	public void setPointB(Point pointB) {
-		this.pointB = pointB;
-	}
-
-	public Point getPointC() {
-		return pointC;
-	}
-
-	public void setPointC(Point pointC) {
-		this.pointC = pointC;
+	public void setPoints(List<Point> points) {
+		this.points = points;
 	}
 
 	public void draw() {
-		System.out.println(this.getPointA().getX() + " :::: " + this.getPointA().getY());
-		System.out.println(this.getPointB().getX() + " :::: " + this.getPointB().getY());
-		System.out.println(this.getPointC().getX() + " :::: " + this.getPointC().getY());
+		for(Point point : points) {
+			System.out.println(point.getX() + " :::: " + point.getY());
+		}
 	}
 }
