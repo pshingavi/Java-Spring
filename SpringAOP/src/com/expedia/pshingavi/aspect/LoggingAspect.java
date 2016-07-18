259105ccd55d425631be70be59307f50f4f8462d
Preetam Shingavi <pshingavi@expedia.com>
2016-07-18 13:14:48 -0700

    AOP via XML

--- SpringAOP/src/com/expedia/pshingavi/aspect/LoggingAspect.java
+++ SpringAOP/src/com/expedia/pshingavi/aspect/LoggingAspect.java
@@ -12,41 +12,10 @@ import org.aspectj.lang.annotation.Pointcut;
 
 import com.expedia.pshingavi.model.Circle;
 
-@Aspect
+//@Aspect	// transfered to spring.xml config
 public class LoggingAspect {
 	
-	@Before("allCircleMethods()")	// use pointcut
-	public void LoggingAdvice(JoinPoint joinPoint) {	// joinPoint is place which triggered this (a method)
-		System.out.println(joinPoint.toString());	// execution(String com.expedia.pshingavi.model.Circle.getName())
-		Circle circle = (Circle) joinPoint.getTarget();
-	}
-	
-	@After("args(String)")
-	public void stringArgsMethod() {
-		System.out.println("Method with String argument has been called.");
-		// Method with String argument has been called.
-	}
-	
-	@After("args(name)")	// Like finally, runs even after exception
-	public void stringArgsMethodParam(String name) {
-		System.out.println("After : Method with String argument has been called : " + name);
-		// Method with String argument has been called : Dummy circle
-	}
-	
-	@AfterReturning(pointcut="args(name)", returning="returnString")	// Not executed if exception
-	public void stringArgsMethodParamReturn(String name, String returnString) {
-		System.out.println("After returning: Method takes : " + name);
-		System.out.println("After returning: Method returns : " + returnString);
-		// Method with String argument has been called : Dummy circle
-	}
-	
-	@AfterThrowing(pointcut="args(name)", throwing="ex")	// run when method execution failed
-	public void stringArgsMethodParamThrow(String name, RuntimeException ex) {
-		System.out.println("Exception thrown : " + ex);
-		// Method with String argument has been called : Dummy circle
-	}
-	
-	@Around("@annotation(com.expedia.pshingavi.aspect.Loggable)")
+	//@Around("@annotation(com.expedia.pshingavi.aspect.Loggable)")
 	public Object myAroundAdvice(ProceedingJoinPoint proceedingJointPoint) {
 		Object returnValue = null;
 		// Before
@@ -64,15 +33,11 @@ public class LoggingAspect {
 		}
 		return returnValue;
 	}
-	
-	@Pointcut("execution(public * get*())")	// for all model and sub packages
-	public void allGetters() {}
 
-	// Naming convention has helped
-	//@Pointcut("execution(* com.expedia.pshingavi.service.*Service.*(..))")
+	// Moved to xml
 	
-	
-	@Pointcut("within(com.expedia.pshingavi.model.Circle)")	// for all model and sub packages
-	public void allCircleMethods() {}
+	/*@Pointcut("execution(* get*())")
+	public void allGetters() {}
+*/
 
 }
