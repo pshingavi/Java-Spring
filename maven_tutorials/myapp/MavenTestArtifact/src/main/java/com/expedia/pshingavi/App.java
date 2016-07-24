f385217adc4660777df4f846b2eac54ed8defbe9
Preetam Shingavi <preetams3@gmail.com>
2016-07-23 20:17:56 -0700

    Maven tutorial

---
+++ maven_tutorials/myapp/MavenTestArtifact/src/main/java/com/expedia/pshingavi/App.java
@@ -0,0 +1,16 @@
+package com.expedia.pshingavi;
+
+import org.slf4j.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {	
+        //System.out.println( "Hello World!" );
+		Logger logger = LoggerFactory.getLogger(App.class);
+		logger.info("My INFO log");
+    }
+}
