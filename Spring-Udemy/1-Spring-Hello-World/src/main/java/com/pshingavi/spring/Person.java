7b69401589e29d98c390030820c45ac4416431bf
Preetam Shingavi <preetams3@gmail.com>
2016-07-24 15:17:28 -0700

    1-Spring-Hello-World

---
+++ Spring-Udemy/1-Spring-Hello-World/src/main/java/com/pshingavi/spring/Person.java
@@ -0,0 +1,10 @@
+package com.pshingavi.spring;
+
+/**
+ * Created by pshingavi on 7/24/2016.
+ */
+public class Person {
+    public void speak() {
+        System.out.println("Person speaking");
+    }
+}
