0c72ce50e9dd1df0ae0f6e308dc50e63af148bab
Preetam Shingavi <preetams3@gmail.com>
2016-07-31 14:19:03 -0700

    NamedParametersJdbcTemplate instead of just JdbcTemplate

--- Spring-Udemy/13-Spring-DAO/src/main/java/com/pshingavi/spring/dao/OffersDAO.java
+++ Spring-Udemy/13-Spring-DAO/src/main/java/com/pshingavi/spring/dao/OffersDAO.java
@@ -7,8 +7,9 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import com.pshingavi.spring.model.Offer;
@@ -16,11 +17,11 @@ import com.pshingavi.spring.model.Offer;
 @Component("offersDAO")
 public class OffersDAO {
 
-	private JdbcTemplate jdbc;
+	private NamedParameterJdbcTemplate jdbc;
 	
 	@Autowired
 	public void setDataSource(DataSource jdbc) {
-		this.jdbc = new JdbcTemplate(jdbc);
+		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
 	}
 
 	public List<Offer> getOffers() {
@@ -35,6 +36,20 @@ public class OffersDAO {
 				return offer;
 			}
 			
+		});
+	}
+	
+	public Offer getOfferById(int id) {
+		MapSqlParameterSource params = new MapSqlParameterSource("id", id);
+		return jdbc.queryForObject("select * from offers where id = :id", params, new RowMapper<Offer>() {
+			public Offer mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+				Offer offer = new Offer();
+				offer.setId(resultSet.getInt("id"));
+				offer.setName(resultSet.getString("name"));
+				offer.setEmail(resultSet.getString("email"));
+				offer.setText(resultSet.getString("text"));
+				return offer;
+			}
 		});
 	}
 }
