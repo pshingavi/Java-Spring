8a054ab5aac2ddb298ec6ec785cc22de8ddc4b1f
Preetam Shingavi <preetams3@gmail.com>
2016-07-30 17:52:09 -0700

    Spring property values via @Value in setter param, @Autowired above setter method and @Component above bean class

--- Spring-Udemy/11-Spring-Autowiring/src/main/java/com/pshingavi/spring/logger/FileWriter.java
+++ Spring-Udemy/11-Spring-Autowiring/src/main/java/com/pshingavi/spring/logger/FileWriter.java
@@ -1,12 +1,22 @@
 package com.pshingavi.spring.logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FileWriter implements LogWriter {
 
+	private String message = "default";
+	
+	// @Value, @Autowired and @Component (above class) gives a simulation of <bean><property name="message" value="something">
+	@Autowired
+	public void setMessage(@Value("Injected message") String message) {
+		this.message = message;
+	}
+	
 	public void write() {
-		System.out.println("Writing to file");
+		System.out.println("Writing to file message : " + message);
 	}
 
 }
