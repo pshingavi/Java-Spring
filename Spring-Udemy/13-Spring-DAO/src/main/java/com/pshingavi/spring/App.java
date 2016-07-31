68e94498b278891c0198daaf40568724711a03c0
Preetam Shingavi <preetams3@gmail.com>
2016-07-31 14:06:11 -0700

    Exception handling using spring's DataAccessException generic class

--- Spring-Udemy/13-Spring-DAO/src/main/java/com/pshingavi/spring/App.java
+++ Spring-Udemy/13-Spring-DAO/src/main/java/com/pshingavi/spring/App.java
@@ -1,5 +1,14 @@
 package com.pshingavi.spring;
 
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+
+import com.pshingavi.spring.dao.OffersDAO;
+import com.pshingavi.spring.model.Offer;
+
 /**
  * Hello world!
  *
@@ -8,6 +17,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        OffersDAO offersDAO = ctx.getBean("offersDAO", OffersDAO.class);
+        try {
+        	List<Offer> allOffers = offersDAO.getOffers();
+        	for(Offer offer : allOffers) {
+	        	System.out.println(offer);
+	        }
+		} catch (DataAccessException e) {	// Any DB related exceptions are thrown into DataAccessException parent exception class
+			System.out.println(e.getMessage());
+			System.out.println(e.getClass());	// Exact class of exception
+		}
+        ((ClassPathXmlApplicationContext)ctx).close();
     }
 }
--- Spring-Udemy/13-Spring-DAO/src/main/java/com/pshingavi/spring/dao/OffersDAO.java
+++ Spring-Udemy/13-Spring-DAO/src/main/java/com/pshingavi/spring/dao/OffersDAO.java
@@ -1,16 +1,19 @@
 package com.pshingavi.spring.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import com.pshingavi.spring.model.Offer;
 
-@Component
+@Component("offersDAO")
 public class OffersDAO {
 
 	private JdbcTemplate jdbc;
@@ -21,6 +24,17 @@ public class OffersDAO {
 	}
 
 	public List<Offer> getOffers() {
-		return null;
+		return jdbc.query("select * from offers", new RowMapper<Offer>() {
+
+			public Offer mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+				Offer offer = new Offer();
+				offer.setId(resultSet.getInt("id"));
+				offer.setName(resultSet.getString("name"));
+				offer.setEmail(resultSet.getString("email"));
+				offer.setText(resultSet.getString("text"));
+				return offer;
+			}
+			
+		});
 	}
 }
