a4b2a696e283c988640082b64921ba29011cf4c9
Preetam Shingavi <preetams3@gmail.com>
2016-08-15 23:35:34 -0700

    Use of Model as parameter in @RequestMapping method

--- Spring-Udemy/15-Springify-Web/src/com/pshingavi/controllers/OffersController.java
+++ Spring-Udemy/15-Springify-Web/src/com/pshingavi/controllers/OffersController.java
@@ -6,6 +6,7 @@ package com.pshingavi.controllers;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,12 +17,18 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class OffersController {
 
-	@RequestMapping("/")
+	/*@RequestMapping("/")
 	public ModelAndView showHome() {
 		// ModelAndView is only for the request scope
 		ModelAndView mv = new ModelAndView("home");
 		Map<String, Object> model = mv.getModel();
 		model.put("name", "<b>Preetam</b>");	// This will be interpreted as bold tags in HTML
 		return mv;
+	}*/
+	
+	@RequestMapping("/")
+	public String showHome(Model model) {
+		model.addAttribute("name", "Preetam");
+		return "home";
 	}
 }
